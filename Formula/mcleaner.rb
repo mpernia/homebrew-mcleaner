7805b62b35d8cce4bc60e14f2713782d89c04387
Maikel Enrique Pernía Matos <25876961+mpernia@users.noreply.github.com>
2026-03-28 00:56:57 +0100

    Update SHA256 checksum for mcleaner
    Updated the SHA256 checksum for version 0.1.0.

--- Formula/mcleaner.rb
+++ Formula/mcleaner.rb
@@ -2,7 +2,7 @@ class Mcleaner < Formula
   desc "A macOS CLI/TUI cleaner tool"
   homepage "https://github.com/mpernia/mcleaner"
   url "https://github.com/mpernia/mcleaner/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "6d49be4c834b0054c5fd188d86de2eaae97fd94791d6531ffc86741be2e97846"
+  sha256 "96cdf4e1dc03aab73b6c48c02d63659f10579290e0142882b716807242f021b6"
   license "MIT"
   version "0.1.0"
 
